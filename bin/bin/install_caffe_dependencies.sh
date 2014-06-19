648fd2a54dd6d35136e3b093a587dcdf93c4ae11
Miquel Perello Nieto <perellm1@pc240.ics.hut.fi>
2014-06-19 18:20:59 +0300

    specify the installation folder

--- bin/bin/install_caffe_dependencies.sh
+++ bin/bin/install_caffe_dependencies.sh
@@ -1,7 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o errexit
+# set -o xtrace
+
+__DIR__="$(cd "$(dirname "${0}")"; echo $(pwd))"
+__BASE__="$(basename "${0}")"
+__FILE__="${__DIR__}/${__BASE__}"
+
+PREFIX="${1:-Undefined}"
+
+[ $# -eq 0 ] && { echo "Usage: $0 installation_dir" ; exit 1; }
+
+# set -o nounset
 
 INITIAL_PWD=${PWD}
-PREFIX=/share/imagedb/perellm1/usr
 LEVELDB=true
 PROTOBUF=true
 GLOG=true
