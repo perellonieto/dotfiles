08c43478a8215ab312fb6d9e3bec772d3b8fa71b
Miquel Perelló Nieto <perello.nieto@gmail.com>
2017-01-03 12:07:43 +0000

    Connect external monitor now is agnostic to the monitor name

--- bin/bin/XF86Display.sh
+++ bin/bin/XF86Display.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 PRI="LVDS1"
-EXT="VGA1"
+connectedOutputs=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/" | grep -v ${PRI})
+
+EXT=${connectedOutputs[0]}
+
 position='--right-of' # '--above' # --right-of
 
-if (xrandr | grep "${EXT} connected" > /dev/null)
+if [[ -z ${EXT// } ]]
 then
-    echo "${EXT} connected"
-    xrandr --output $PRI --auto --primary --output $EXT --auto ${position} $PRI
-else
-    echo "${EXT} not connected"
     xrandr --auto
+else
+    xrandr --output $PRI --auto --primary --output $EXT --auto ${position} $PRI
 fi
