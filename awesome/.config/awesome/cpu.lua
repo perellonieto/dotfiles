9d1bfd2d99b07fe90a85d6a20b0ca241457eab27
Miquel Perelló Nieto <perello.nieto@gmail.com>
2018-11-30 18:19:14 +0000

    Solved problem with cpu

--- awesome/.config/awesome/cpu.lua
+++ awesome/.config/awesome/cpu.lua
@@ -9,27 +9,50 @@
 -- M is the returned object containing the function which gathers the information
 local M = {}
 
+local total_prev = 0
+local idle_prev = 0
+
 function M.getCpu(mid, high)
 
-    local cpu_out = {'CPU'}
-    local fd = io.popen("grep 'cpu' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} {print int(usage)}'", "r")
+    local cpu_out = {'cpu'}
+    -- user(1) nice(2) system(3) idle(4) iowait(5) irq(6) softirq(7) steal(8)
+    -- guest(9) guest_nice(10)
+    local fd = io.popen("grep 'cpu ' /proc/stat", "r")
+
     local line = fd:read()
 
-    while line do
-        cpu_usage = tonumber(line)
-        if cpu_usage>high then
-            color = "<b><span color='red'>"
-        elseif cpu_usage>mid then
-            color = "<b><span color='orange'>"
-        else
-            color = "<b><span color='green'>"
-        end
+    local user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice = line:match('(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)%s(%d+)')
 
-        table.insert(cpu_out, color .. cpu_usage .. "%</span></b>")
+    user = tonumber(user)
+    nice = tonumber(nice)
+    system = tonumber(system)
+    idle = tonumber(idle)
+    iowait = tonumber(iowait)
+    irq = tonumber(irq)
+    softirq = tonumber(softirq)
+    steal = tonumber(steal)
+    guest = tonumber(guest)
+    guest_nice = tonumber(guest_nice)
 
-        line = fd:read()
+    local total = user + nice + system + idle + iowait + irq + softirq + steal
+
+    local diff_idle = idle - idle_prev
+    local diff_total = total - total_prev
+    local diff_usage = (1000 * (diff_total - diff_idle) / diff_total + 5) / 10
+
+    if diff_usage>high then
+        color = 'red'
+    elseif diff_usage>mid then
+        color = 'orange'
+    else
+        color = 'green'
     end
-    return table.concat(cpu_out, "|")
+
+    table.insert(cpu_out, "<b><span color='" .. color .. "'>" .. math.floor(diff_usage) .. "%</span></b>")
+
+    total_prev = total
+    idle_prev = idle
+    return table.concat(cpu_out)
 
 end
 
