04ef9fedf7433bd7d9b0e22610542002659e8d25
Miquel Perelló Nieto <perello.nieto@gmail.com>
2019-08-16 12:57:10 +0100

    Modified switch to empty workspaces

---
+++ i3/.config/i3/move_to_empty_workspace.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+current_workspaces=($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -n))
+printf "%s\n" "${current_workspaces[@]}"
+
+new_workspace=0
+for i in {1..100}
+do
+    found=0
+    for e in "${current_workspaces[@]}"; do
+        if [ "$i" == "$e" ]; then
+            found=1
+            break
+        fi
+    done
+    if [ $found -eq 0 ]; then
+        new_workspace=$i
+        break
+    fi
+done
+
+i3-msg move container to workspace $new_workspace
+i3-msg workspace $new_workspace
