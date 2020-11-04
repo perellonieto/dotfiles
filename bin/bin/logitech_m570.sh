3cdb89c15ee284d8f4c1f81da310b3300946218e
Miquel Perelló Nieto <perello.nieto@gmail.com>
2020-11-04 16:39:14 +0000

    Added UoB laptop config

---
+++ bin/bin/logitech_m570.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Check the parameters
+#xinput --list-props "Logitech M570"
+#xinput --set-prop "Logitech M570" "Device Accel Constant Deceleration" 1.5
+xinput --set-prop "Logitech M570" "libinput Accel Speed" 1.0
